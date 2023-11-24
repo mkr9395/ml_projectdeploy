fc5740c6047425c4b197b4dfcb66779387728560
Mohit Kumar <mkr9395@gmail.com>
2023-11-25 00:17:48 +0530

    "data_ingestion"

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,3 +3,57 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+
+'''
+The dataclasses module in Python provides a decorator and functions for automatically adding special methods 
+such as __init__() and __repr__() to user-defined classes. 
+This module was introduced in Python 3.7 to simplify the creation of classes that primarily store data.
+'''
+
+from dataclasses import dataclass
+
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts','train.csv') # path to save train data
+    test_data_path:str = os.path.join('artifacts','test.csv') # path to save test data
+    raw_data_path:str = os.path.join('artifacts','data.csv') # path to save initial data
+    
+    
+class Dataingestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig() # all 3 paths will be saved in ingestion_config
+    
+    # intitate data from outer source eg: mongodb,sql server    
+    def initiate_data_ingestion(self): 
+        logging.info("Enter the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            logging.info("train-test split initiated")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            
+            logging.info("Ingestion of data completed")
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj = Dataingestion()
+    obj.initiate_data_ingestion()
+    
+############### use this command to run the file -> python -m src.components.data_ingestion         
