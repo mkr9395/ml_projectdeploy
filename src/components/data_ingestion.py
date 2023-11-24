784208bfeb3b4945420951aae625119bd7a356a4
Mohit Kumar <mkr9395@gmail.com>
2023-11-25 00:24:33 +0530

    "git-ignore"

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -29,7 +29,7 @@ class Dataingestion:
     def initiate_data_ingestion(self): 
         logging.info("Enter the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook\data\stud.csv') # reading the data
             logging.info('Read the dataset as dataframe')
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
