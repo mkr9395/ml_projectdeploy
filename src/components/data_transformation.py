c39e333582cd7830b59392b9d0a05d275da9b25c
Mohit Kumar <mkr9395@gmail.com>
2023-11-26 02:08:24 +0530

    "data ingestion error solved"

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -113,19 +113,17 @@ class Datatransformation:
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
 
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            train_arr = np.c_[ input_feature_train_arr, np.array(target_feature_train_df) ]
+            test_arr = np.c_[ input_feature_test_arr, np.array(target_feature_test_df) ]
 
             logging.info(f"Saved preprocessing object.")
-            '''
-            save_object(
+            
+            save_obj(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
 
             )
-            '''
+            
             return (
                 train_arr,
                 test_arr,
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import dill
 
 import pandas as pd
 import numpy as np
